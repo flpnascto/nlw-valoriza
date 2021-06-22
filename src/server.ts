9f9388dd9ed362029a6915435a271e989b54e5af
Felipe Nascimento <flpnascto@gmail.com>
2021-06-21 22:38:08 -0300

    create route for create new user

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import 'reflect-metadata';
 import express from 'express';
+import { routes } from './routes';
 
 import './database';
 
 const PORT = 3000;
 const app = express();
+
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(PORT, () => console.log('Server Online!'))
