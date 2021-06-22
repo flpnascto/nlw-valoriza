5e6f273744c79a8519448a935555627a3afa81a2
Felipe Nascimento <flpnascto@gmail.com>
2021-06-21 22:12:11 -0300

    create CreateUser service

---
+++ src/services/CreateUserServices.ts
@@ -0,0 +1,31 @@
+import { UsersRepositories } from "../repositories/UserRepositories";
+
+interface IUserRequest {
+  name: string;
+  email: string;
+  admin?: boolean;
+}
+
+class CreateUserService {
+  async execute({ name, email, admin }: IUserRequest) {
+    const usersRepository = new UsersRepositories();
+
+    const userAlreadyExists = await usersRepository.findOne({ email });
+
+    if (userAlreadyExists) {
+      throw new Error('User already exists');
+    };
+
+    const user = usersRepository.create({
+      name,
+      email,
+      admin,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  };
+};
+
+export { CreateUserService }
