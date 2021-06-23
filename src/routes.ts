e03f2390e6b0738fe021056ace98ac8215b402ae
Felipe Nascimento <flpnascto@gmail.com>
2021-06-22 22:03:04 -0300

    create Tag route

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { CreateUserController } from './controllers/CreateUserController';
-
+import { CreateTagController } from './controllers/CreateTagController';
 const routes = Router();
 
 const createUserController = new CreateUserController();
+const createTagController = new CreateTagController();
 
 routes.post('/users', createUserController.handle);
-
+routes.post('/tags', createTagController.handle);
 export { routes };
